42554c87e7bbb598b22703878506010e2a812f97
hayas1 <h4ystack@gmail.com>
2021-01-03 23:55:30 +0900

    implement dfs order

--- src/graph/search.rs
+++ src/graph/search.rs
@@ -2,7 +2,7 @@ use std::collections::{HashSet, VecDeque};
 
 use crate::graph::structure::AdjacencyList;
 
-/// **O(n)**, return bfs order
+/// **O(n)**, return breadth first search order
 pub fn bfs<W, D>(graph: AdjacencyList<W, D>, start: usize) -> Vec<usize> {
     let mut queue = VecDeque::new();
     let mut touched = HashSet::new();
@@ -14,6 +14,24 @@ pub fn bfs<W, D>(graph: AdjacencyList<W, D>, start: usize) -> Vec<usize> {
         let neighbors = &graph[node];
         for &nei in neighbors {
             queue.push_back(nei);
+            touched.insert(nei);
+        }
+    }
+    order
+}
+
+/// **O(n)**, return depth first search order
+pub fn dfs<W, D>(graph: AdjacencyList<W, D>, start: usize) -> Vec<usize> {
+    let mut stack = Vec::new();
+    let mut touched = HashSet::new();
+    let mut order = Vec::new();
+    stack.push(start);
+    touched.insert(start);
+    while let Some(node) = stack.pop() {
+        order.push(node);
+        let neighbors = &graph[node];
+        for &nei in neighbors {
+            stack.push(nei);
             touched.insert(nei);
         }
     }
