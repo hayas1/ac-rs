6ea19d60b251334092be13dd1312929c998958e2
hayas1 <h4ystack@gmail.com>
2021-01-03 23:35:26 +0900

    implement adjacency list

--- src/graph/structure.rs
+++ src/graph/structure.rs
@@ -1,19 +1,19 @@
-use std::{collections::HashMap, marker::PhantomData};
+use std::collections::{HashMap, HashSet};
+use std::marker::PhantomData;
+use std::ops::Index;
 
-#[derive(Clone, Debug)]
 pub enum Undirected {}
 
-#[derive(Clone, Debug)]
 pub enum Directed {}
 
-/// graph
+/// graph structure
 pub struct AdjacencyList<W, D> {
-    list: Vec<Vec<usize>>,
+    neighbors: HashMap<usize, HashSet<usize>>,
     weight: HashMap<(usize, usize), W>,
     directed: PhantomData<D>,
 }
 
-/// unweighted undirected graph
+/// unweighted undirected graph structure
 impl AdjacencyList<(), Undirected> {
     /// **O(m)**, convert sequence of edges(unweighted, undirected) to adjacency list
     pub fn new_unweighted_undirected((n, _m): (usize, usize), e: &[(usize, usize)]) -> Self {
@@ -22,7 +22,7 @@ impl AdjacencyList<(), Undirected> {
     }
 }
 
-/// unweighted directed graph
+/// unweighted directed graph structure
 impl AdjacencyList<(), Directed> {
     /// **O(m)**, convert sequence of edges(unweighted, undirected) to adjacency list
     pub fn new_unweighted_directed((n, _m): (usize, usize), e: &[(usize, usize)]) -> Self {
@@ -31,62 +31,87 @@ impl AdjacencyList<(), Directed> {
     }
 }
 
-/// weighted undirected graph
+/// weighted undirected graph structure
 impl<W: Copy> AdjacencyList<W, Undirected> {
     /// **O(m)**, convert sequence of edges(weighted, undirected) to adjacency list
     pub fn new_weighted_undirected((n, _m): (usize, usize), e: &[(usize, usize, W)]) -> Self {
-        let mut edges = vec![Vec::new(); n];
-        let mut weight_map = HashMap::new();
+        let mut weight = HashMap::new();
+        let mut neighbors: HashMap<_, _> = (0..n).map(|u| (u, HashSet::new())).collect();
         for &(u, v, w) in e {
-            edges[u].push(v);
-            edges[v].push(u);
-            weight_map.insert((u, v), w);
-            weight_map.insert((v, u), w);
+            neighbors
+                .get_mut(&u)
+                .unwrap_or_else(|| panic!("unexpected node: {}", u)) // expect will cost format every time
+                .insert(v);
+            neighbors
+                .get_mut(&v)
+                .unwrap_or_else(|| panic!("unexpected node: {}", u)) // expect will cost format every time
+                .insert(u);
+            weight.insert((u, v), w);
+            weight.insert((v, u), w);
         }
         Self {
-            list: edges,
-            weight: weight_map,
+            neighbors,
+            weight,
             directed: PhantomData,
         }
     }
 }
 
-/// weighted directed graph
+/// weighted directed graph structure
 impl<W: Copy> AdjacencyList<W, Directed> {
     /// **O(m)**, convert sequence of edges(weighted, undirected) to adjacency list
     pub fn new_weighted_directed((n, _m): (usize, usize), e: &[(usize, usize, W)]) -> Self {
-        let mut edges = vec![Vec::new(); n];
-        let mut weight_map = HashMap::new();
+        let mut weight = HashMap::new();
+        let mut neighbors: HashMap<_, _> = (0..n).map(|u| (u, HashSet::new())).collect();
         for &(u, v, w) in e {
-            edges[u].push(v);
-            weight_map.insert((u, v), w);
+            neighbors
+                .get_mut(&u)
+                .unwrap_or_else(|| panic!("unexpected node: {}", u)) // expect will cost format every time
+                .insert(v);
+            weight.insert((u, v), w);
         }
         Self {
-            list: edges,
-            weight: weight_map,
+            neighbors,
+            weight,
             directed: PhantomData,
         }
     }
 }
 
-/// weighted graph
+/// weighted graph structure
 impl<W: Copy, D> AdjacencyList<W, D> {
     /// **O(1)**, get weight of edge
     pub fn weight(&self, u: usize, v: usize) -> W {
         self.weight[&(u, v)]
     }
 }
+impl<W: Copy, D> Index<(usize, usize)> for AdjacencyList<W, D> {
+    type Output = W;
 
-/// graph
+    #[inline]
+    fn index(&self, (u, v): (usize, usize)) -> &Self::Output {
+        &self.weight[&(u, v)]
+    }
+}
+
+/// graph structure
 impl<W, D> AdjacencyList<W, D> {
     /// **O(1)**, get node's neighbors list reference
-    pub fn neighbors(&self, node: usize) -> &Vec<usize> {
-        &self.list[node]
+    pub fn neighbors(&self, node: usize) -> &HashSet<usize> {
+        &self.neighbors[&node]
     }
 
     /// **O(1)**, number of nodes
-    pub fn nodes(&self) -> usize {
-        self.list.len()
+    pub fn nodes_len(&self) -> usize {
+        self.neighbors.len()
+    }
+}
+impl<W: Copy, D> Index<usize> for AdjacencyList<W, D> {
+    type Output = HashSet<usize>;
+
+    #[inline]
+    fn index(&self, node: usize) -> &Self::Output {
+        &self.neighbors[&node]
     }
 }
 
@@ -103,100 +128,121 @@ mod tests {
         let (n, m) = (5, 6);
         let e = vec![(0, 1), (1, 2), (2, 0), (0, 3), (3, 4), (4, 0)];
         let adjacency_list = AdjacencyList::new_unweighted_undirected((n, m), &e);
-        assert_eq!(adjacency_list.nodes(), 5);
-        assert_eq!(adjacency_list.neighbors(0), &[1, 2, 3, 4]);
-        assert_eq!(adjacency_list.neighbors(1), &[0, 2]);
-        assert_eq!(adjacency_list.neighbors(2), &[0, 1]);
-        assert_eq!(adjacency_list.neighbors(3), &[0, 4]);
-        assert_eq!(adjacency_list.neighbors(4), &[0, 3]);
+        assert_eq!(adjacency_list.nodes_len(), 5);
+        assert_eq!(
+            adjacency_list.neighbors(0),
+            &[1, 2, 3, 4].iter().cloned().collect()
+        );
+        assert_eq!(
+            adjacency_list.neighbors(1),
+            &[0, 2].iter().cloned().collect()
+        );
+        assert_eq!(
+            adjacency_list.neighbors(2),
+            &[0, 1].iter().cloned().collect()
+        );
+        assert_eq!(adjacency_list[3], [0, 4].iter().cloned().collect());
+        assert_eq!(adjacency_list[4], [0, 3].iter().cloned().collect());
+        assert_eq!(adjacency_list[(1, 2)], ());
+        assert_eq!(adjacency_list.weight(1, 2), ());
     }
-    // #[test]
-    // fn test_unweighted_directed() {
-    //     // 1-2
-    //     // \ /
-    //     //  0
-    //     // / \
-    //     // 3-4
-    //     let (n, m) = (5, 6);
-    //     let e = vec![(0, 1), (1, 2), (2, 0), (0, 3), (3, 4), (4, 0)];
-    //     let adjacency_list = AdjacencyList::new_directed((n, m), &e);
-    //     assert_eq!(adjacency_list.len(), 5);
-    //     assert_eq!(adjacency_list[&0], [1, 3].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&1], [2].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&2], [0].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&3], [4].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&4], [0].iter().cloned().collect());
-    // }
 
-    // #[test]
-    // fn test_weighted_undirected() {
-    //     //     3
-    //     //    1-2
-    //     //  1 \ / 2
-    //     //     0
-    //     //  3 / \ 4
-    //     //    3-4
-    //     //     7
-    //     let (n, m) = (5, 6);
-    //     let e = vec![
-    //         (0, 1, 1),
-    //         (1, 2, 3),
-    //         (2, 0, 2),
-    //         (0, 3, 3),
-    //         (3, 4, 7),
-    //         (4, 0, 4),
-    //     ];
-    //     let adjacency_list = weighted_undirected_to_adjacency_list((n, m), &e);
-    //     assert_eq!(adjacency_list.len(), 5);
-    //     assert_eq!(
-    //         adjacency_list[&0],
-    //         [(1, 1), (2, 2), (3, 3), (4, 4)].iter().cloned().collect()
-    //     );
-    //     assert_eq!(
-    //         adjacency_list[&1],
-    //         [(0, 1), (2, 3)].iter().cloned().collect()
-    //     );
-    //     assert_eq!(
-    //         adjacency_list[&2],
-    //         [(0, 2), (1, 3)].iter().cloned().collect()
-    //     );
-    //     assert_eq!(
-    //         adjacency_list[&3],
-    //         [(0, 3), (4, 7)].iter().cloned().collect()
-    //     );
-    //     assert_eq!(
-    //         adjacency_list[&4],
-    //         [(0, 4), (3, 7)].iter().cloned().collect()
-    //     );
-    // }
+    #[test]
+    fn test_unweighted_directed() {
+        // 1-2
+        // \ /
+        //  0
+        // / \
+        // 3-4
+        let (n, m) = (5, 6);
+        let e = vec![(0, 1), (1, 2), (2, 0), (0, 3), (3, 4), (4, 0)];
+        let adjacency_list = AdjacencyList::new_unweighted_directed((n, m), &e);
+        assert_eq!(adjacency_list.nodes_len(), 5);
+        assert_eq!(
+            adjacency_list.neighbors(0),
+            &[1, 3].iter().cloned().collect()
+        );
+        assert_eq!(adjacency_list.neighbors(1), &[2].iter().cloned().collect());
+        assert_eq!(adjacency_list.neighbors(2), &[0].iter().cloned().collect());
+        assert_eq!(adjacency_list[3], [4].iter().cloned().collect());
+        assert_eq!(adjacency_list[4], [0].iter().cloned().collect());
+        assert_eq!(adjacency_list[(3, 4)], ());
+        assert_eq!(adjacency_list.weight(3, 4), ());
+    }
 
-    // #[test]
-    // fn test_weighted_directed() {
-    //     //     3
-    //     //    1-2
-    //     //  1 \ / 2
-    //     //     0
-    //     //  3 / \ 4
-    //     //    3-4
-    //     //     7
-    //     let (n, m) = (5, 6);
-    //     let e = vec![
-    //         (0, 1, 1),
-    //         (1, 2, 3),
-    //         (2, 0, 2),
-    //         (0, 3, 3),
-    //         (3, 4, 7),
-    //         (4, 0, 4),
-    //     ];
-    //     let adjacency_list = weighted_directed_to_adjacency_list((n, m), &e);
-    //     assert_eq!(adjacency_list.len(), 5);
-    //     assert_eq!(
-    //         adjacency_list[&0],
-    //         [(1, 1), (3, 3)].iter().cloned().collect()
-    //     );
-    //     assert_eq!(adjacency_list[&1], [(2, 3)].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&2], [(0, 2)].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&3], [(4, 7)].iter().cloned().collect());
-    //     assert_eq!(adjacency_list[&4], [(0, 4)].iter().cloned().collect());
-    // }
+    #[test]
+    fn test_weighted_undirected() {
+        //     3
+        //    1-2
+        //  1 \ / 2
+        //     0
+        //  3 / \ 4
+        //    3-4
+        //     7
+        let (n, m) = (5, 6);
+        let e = vec![
+            (0, 1, 1),
+            (1, 2, 3),
+            (2, 0, 2),
+            (0, 3, 3),
+            (3, 4, 7),
+            (4, 0, 4),
+        ];
+        let adjacency_list = AdjacencyList::new_weighted_undirected((n, m), &e);
+        assert_eq!(adjacency_list.nodes_len(), 5);
+        assert_eq!(
+            adjacency_list.neighbors(0),
+            &[1, 2, 3, 4].iter().cloned().collect()
+        );
+        assert_eq!(
+            adjacency_list.neighbors(1),
+            &[0, 2].iter().cloned().collect()
+        );
+        assert_eq!(
+            adjacency_list.neighbors(2),
+            &[0, 1].iter().cloned().collect()
+        );
+        assert_eq!(adjacency_list[3], [0, 4].iter().cloned().collect());
+        assert_eq!(adjacency_list[4], [0, 3].iter().cloned().collect());
+        assert_eq!(adjacency_list.weight(0, 1), 1);
+        assert_eq!(adjacency_list.weight(1, 0), 1);
+        assert_eq!(adjacency_list.weight(3, 4), 7);
+        assert_eq!(adjacency_list[(0, 1)], 1);
+        assert_eq!(adjacency_list[(1, 0)], 1);
+        assert_eq!(adjacency_list[(3, 4)], 7);
+    }
+
+    #[test]
+    fn test_weighted_directed() {
+        //     3
+        //    1-2
+        //  1 \ / 2
+        //     0
+        //  3 / \ 4
+        //    3-4
+        //     7
+        let (n, m) = (5, 6);
+        let e = vec![
+            (0, 1, 1),
+            (1, 2, 3),
+            (2, 0, 2),
+            (0, 3, 3),
+            (3, 4, 7),
+            (4, 0, 4),
+        ];
+        let adjacency_list = AdjacencyList::new_weighted_directed((n, m), &e);
+        assert_eq!(adjacency_list.nodes_len(), 5);
+        assert_eq!(
+            adjacency_list.neighbors(0),
+            &[1, 3].iter().cloned().collect()
+        );
+        assert_eq!(adjacency_list.neighbors(1), &[2].iter().cloned().collect());
+        assert_eq!(adjacency_list.neighbors(2), &[0].iter().cloned().collect());
+        assert_eq!(adjacency_list[3], [4].iter().cloned().collect());
+        assert_eq!(adjacency_list[4], [0].iter().cloned().collect());
+        assert_eq!(adjacency_list.weight(0, 1), 1);
+        assert_eq!(adjacency_list.weight(3, 4), 7);
+        assert_eq!(adjacency_list[(2, 0)], 2);
+        assert_eq!(adjacency_list[(4, 0)], 4);
+    }
 }
