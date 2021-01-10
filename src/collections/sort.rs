48472793d14e4aee4bb7f1596565046d3781adfa
hayas1 <h4ystack@gmail.com>
2021-01-10 19:23:03 +0900

    implement insertion sort

--- src/collections/sort.rs
+++ src/collections/sort.rs
@@ -1,4 +1,4 @@
-/// **O(n^2)**, return stable sorted data by bubble sort.
+/// **O(n^2)**, stable sorted data by bubble sort
 pub fn bubble_sort<T: PartialOrd>(data: &mut [T]) {
     let n = data.len();
     for i in 0..n {
@@ -10,7 +10,7 @@ pub fn bubble_sort<T: PartialOrd>(data: &mut [T]) {
     }
 }
 
-/// **O(n^2)**, return stable sorted data by selection sort.
+/// **O(n^2)**, stable sorted data by selection sort
 pub fn selection_sort<T: Ord>(data: &mut [T]) {
     let n = data.len();
     for i in 0..n {
@@ -21,6 +21,17 @@ pub fn selection_sort<T: Ord>(data: &mut [T]) {
             .unwrap()
             .0;
         data.swap(n - i - 1, argmax);
+    }
+}
+
+/// **O(n + inversion_number(data))**, stable sorted data by insertion sort
+pub fn insertion_sort<T: PartialOrd>(data: &mut [T]) {
+    for i in 0..data.len() {
+        let mut j = i;
+        while j > 0 && data[j - 1] > data[j] {
+            data.swap(j - 1, j);
+            j -= 1;
+        }
     }
 }
 
@@ -39,6 +50,13 @@ mod tests {
     fn selection_sort_test() {
         let mut v = vec![78, 15, 63, 56, 17, 42];
         selection_sort(&mut v);
+        assert_eq!(v, [15, 17, 42, 56, 63, 78]);
+    }
+
+    #[test]
+    fn insertion_sort_test() {
+        let mut v = vec![78, 15, 63, 56, 17, 42];
+        insertion_sort(&mut v);
         assert_eq!(v, [15, 17, 42, 56, 63, 78]);
     }
 }
