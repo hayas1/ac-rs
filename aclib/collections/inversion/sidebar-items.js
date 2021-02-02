1f9c33b8bf082af28110f28b572fd73736a821de
hayas1 <hayas1@users.noreply.github.com>
2021-02-02 05:16:00 +0000

    deploy: bd50ec6692fa1afd22127cd4030535d3a14f9c71

--- aclib/collections/inversion/sidebar-items.js
+++ aclib/collections/inversion/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["inversion_number","O(n log(n)), calculate inversion number, on data such as permutation of 0,1,2,...,n-1"],["inversion_number_with","O(n log(n)), calculate inversion number"]]});
+initSidebarItems({"fn":[["inversion_number","O(n log(n)), calculate inversion number, on data such as permutation of 0,1,2,...,n-1"],["inversion_number_with","O(n log(n)), calculate inversion number for deduplicated data"]]});
