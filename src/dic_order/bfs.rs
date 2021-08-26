6f940f2fbb232a558b64524bb7cceedf4a3f7151
hayas1 <h4ystack@gmail.com>
2021-08-26 20:02:04 +0900

    refactor dic_order bfs

--- src/dic_order/bfs.rs
+++ src/dic_order/bfs.rs
@@ -1,8 +1,5 @@
 use itertools::Itertools;
-use std::{
-    collections::{HashMap, VecDeque},
-    hash::Hash,
-};
+use std::{collections::HashMap, hash::Hash};
 
 /// **O(|candidate| * (kinds of chars)^2)**, generate the kth item in dictionary order <br>
 /// warning: this use factorial, so large candidate will cause overflow <br>
@@ -11,44 +8,45 @@ pub fn kth_dic_order<T>(candidate: &[T], k: usize) -> Vec<T>
 where
     T: Clone + Hash + Eq + Ord,
 {
-    let factorial = (0..=candidate.len())
-        .scan(1, |fac, x| {
-            *fac *= x.max(1);
-            Some(*fac)
-        })
-        .collect_vec();
-    let mut counter = HashMap::new();
-    for c in candidate.iter() {
-        *counter.entry(c.clone()).or_insert(0) += 1;
-    }
-    bfs(&mut counter, k, &factorial).into_iter().collect()
+    let (factorial, mut counter) = (
+        (0..=candidate.len())
+            .scan(1, |fac, x| {
+                *fac *= x.max(1);
+                Some(*fac)
+            })
+            .collect_vec(),
+        candidate.iter().cloned().counts(),
+    );
+    let mut result = Vec::new();
+    bfs(&mut counter, k, &factorial, &mut result);
+    result
 }
 /// **O(|candidate| * |chars|^2)**, (|chars| mean HashMap's keys size), find the kth item in dictionary order
-fn bfs<T>(chars: &mut HashMap<T, usize>, k: usize, factorial: &[usize]) -> VecDeque<T>
+fn bfs<T>(chars: &mut HashMap<T, usize>, k: usize, factorial: &[usize], result: &mut Vec<T>)
 where
     T: Clone + Hash + Eq + Ord,
 {
     if k <= 1 {
-        return chars.iter().sorted().map(|(c, &n)| vec![c.clone(); n]).flatten().collect();
+        return result.extend(
+            chars.iter().sorted().map(|(c, &n)| vec![c.clone(); n]).flatten().collect_vec(),
+        );
     }
     let sorted =
         chars.iter().filter(|&(_, &n)| n > 0).map(|(c, _)| c.clone()).sorted().collect_vec();
-    let permutations: Vec<_> = vec![0]
+    let permutations = vec![0]
         .into_iter()
         .chain(sorted.iter().scan(0, |cum, c| {
             *cum += permutation(c.clone(), &chars, factorial);
             Some(*cum)
         }))
-        .collect();
+        .collect_vec();
     for (c, (&p, &np)) in sorted.iter().zip(permutations.iter().tuple_windows()) {
         if p < k && k <= np {
             *chars.get_mut(c).expect("above added") -= 1;
-            let mut generated = bfs(chars, k - p, factorial);
-            generated.push_front(c.clone());
-            return generated;
+            result.push(c.clone());
+            return bfs(chars, k - p, factorial, result);
         }
     }
-
     unreachable!("maybe possible permutations {} < k({})", permutations[permutations.len() - 1], k);
 }
 /// **O(|chars|)** (|chars| mean HashMap's keys size), calculate permutation if given char is head
@@ -57,11 +55,11 @@ where
     T: Clone + Hash + Eq + Ord,
 {
     let len = chars.values().fold(0, |sum, x| sum + x);
-    if len > 1 {
+    if len < 2 {
+        0
+    } else {
         let divider = chars.iter().fold(1, |prod, (c, &b)| prod * fac[b - (c == &head) as usize]);
         fac[len - 1] / divider
-    } else {
-        0
     }
 }
 
