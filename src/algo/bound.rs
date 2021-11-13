05496b8efd4dd3f4e99ea1b4d3c5d23baa19e747
hayas1 <h4ystack@gmail.com>
2021-11-14 00:37:08 +0900

    modify upper_bound

--- src/algo/bound.rs
+++ src/algo/bound.rs
@@ -1,13 +1,13 @@
-use num::Integer;
-
-/// **O(log(|a|))**, return first index such as a[i] < x
-pub fn upper_bound<T: Clone + PartialOrd + Integer>(a: &[T], x: T) -> usize {
-    let (mut start, mut end) = (0, a.len());
-    while start < end {
-        let mid = (start.clone() + end.clone()) / 2;
+/// **O(log(|a|))**, return the first index i such that a[i] >= x
+pub fn upper_bound<T: PartialOrd>(a: &[T], x: T) -> usize {
+    let (mut start, mut size) = (0, a.len());
+    while size > 1 {
+        let half = size / 2;
+        let mid = start + half;
         if x < a[mid] {
-            end = mid;
+            size = half;
         } else {
+            size -= half + 1;
             start = mid + 1;
         }
     }
@@ -23,12 +23,12 @@ mod tests {
     fn upper_bound_test() {
         let v = vec![1, 2, 4, 5, 7, 8, 10];
         assert_eq!(upper_bound(&v, 0), 0);
-        assert_eq!(upper_bound(&v, 1), 1);
+        assert_eq!(upper_bound(&v, 1), 0);
         assert_eq!(upper_bound(&v, 3), 2);
-        assert_eq!(upper_bound(&v, 4), 3);
+        assert_eq!(upper_bound(&v, 4), 2);
         assert_eq!(upper_bound(&v, 9), 6);
-        assert_eq!(upper_bound(&v, 10), 7);
-        assert_eq!(upper_bound(&v, 100), 7);
+        assert_eq!(upper_bound(&v, 10), 6);
+        assert_eq!(upper_bound(&v, 100), 6);
     }
 
     #[test]
@@ -38,7 +38,7 @@ mod tests {
         let v = vec![10; 1];
         assert_eq!(upper_bound(&v, 1), 0);
         assert_eq!(upper_bound(&v, 9), 0);
-        assert_eq!(upper_bound(&v, 10), 1);
-        assert_eq!(upper_bound(&v, 11), 1);
+        assert_eq!(upper_bound(&v, 10), 0);
+        assert_eq!(upper_bound(&v, 11), 0);
     }
 }
