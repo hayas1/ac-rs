aeaa75c61adac17486eef33c76416ebc91afde88
hayas1 <h4ystack@gmail.com>
2022-05-05 19:18:10 +0900

    make zero division raise exception

--- src/integer/rational.rs
+++ src/integer/rational.rs
@@ -3,24 +3,34 @@ use num::{integer::gcd, Integer};
 #[derive(Clone, Copy, Eq, Ord, Debug)]
 pub struct Rational<T: Integer + Copy> {
     // TODO: Rational<T> should be add/sub/mul/div T and &T and &Rational<T>
-    // TODO: if denominator is 0, rational should not be made
     // TODO: minus operator should be associated only with numerator
     numerator: T,
     denominator: T,
 }
 impl<T: Integer + Copy> Rational<T> {
+    pub fn new(numerator: T, denominator: T) -> Option<Self> {
+        if denominator == T::zero() {
+            None
+        } else {
+            Some(Self { numerator, denominator })
+        }
+    }
     pub fn irreducible(&self) -> Self {
         let gcd = gcd(self.numerator, self.denominator);
-        Self { numerator: self.numerator / gcd, denominator: self.denominator / gcd }
+        Self::new(self.numerator / gcd, self.denominator / gcd)
+            .expect("denominator shouldn't be zero")
     }
     pub fn common(&self, other: &Self) -> (Self, Self) {
         let gcd = gcd(self.numerator, self.denominator);
         let denominator = self.denominator / gcd * other.denominator;
-        let (sn, on) = (
+        let (s_numerator, o_numerator) = (
             denominator / self.denominator * self.numerator,
             denominator / other.denominator * other.numerator,
         );
-        (Self { numerator: sn, denominator }, Self { numerator: on, denominator })
+        (
+            Self::new(s_numerator, denominator).expect("denominator shouldn't be zero"),
+            Self::new(o_numerator, denominator).expect("denominator shouldn't be zero"),
+        )
     }
 }
 impl<T: std::fmt::Display + Integer + Copy> std::fmt::Display for Rational<T> {
@@ -45,7 +55,7 @@ impl<T: Integer + Copy> std::ops::Add for Rational<T> {
     fn add(self, rhs: Self) -> Self::Output {
         let (sc, rc) = self.common(&rhs);
         let (numerator, denominator) = (sc.numerator + rc.numerator, sc.denominator);
-        Self { numerator, denominator }.irreducible()
+        Self::new(numerator, denominator).expect("denominator shouldn't be zero").irreducible()
     }
 }
 impl<T: Integer + Copy> std::ops::AddAssign for Rational<T> {
@@ -58,7 +68,7 @@ impl<T: Integer + Copy> std::ops::Sub for Rational<T> {
     fn sub(self, rhs: Self) -> Self::Output {
         let (sc, rc) = self.common(&rhs);
         let (numerator, denominator) = (sc.numerator - rc.numerator, sc.denominator);
-        Self { numerator, denominator }.irreducible()
+        Self::new(numerator, denominator).expect("denominator shouldn't be zero").irreducible()
     }
 }
 impl<T: Integer + Copy> std::ops::SubAssign for Rational<T> {
@@ -72,7 +82,7 @@ impl<T: Integer + Copy> std::ops::Mul for Rational<T> {
         let (sc, rc) = self.common(&rhs);
         let (numerator, denominator) =
             (sc.numerator * rc.numerator, sc.denominator * rc.denominator);
-        Self { numerator, denominator }.irreducible()
+        Self::new(numerator, denominator).expect("denominator shouldn't be zero").irreducible()
     }
 }
 impl<T: Integer + Copy> std::ops::MulAssign for Rational<T> {
@@ -86,7 +96,7 @@ impl<T: Integer + Copy> std::ops::Div for Rational<T> {
         let (sc, rc) = self.common(&rhs);
         let (numerator, denominator) =
             (sc.numerator * rc.denominator, sc.denominator * rc.numerator);
-        Self { numerator, denominator }.irreducible()
+        Self::new(numerator, denominator).expect("denominator shouldn't be zero").irreducible()
     }
 }
 impl<T: Integer + Copy> std::ops::DivAssign for Rational<T> {
@@ -100,77 +110,91 @@ mod tests {
 
     use super::*;
     #[test]
+    fn test_new() {
+        assert_eq!(Rational::new(12, 36), Some(Rational { numerator: 12, denominator: 36 }));
+        assert_eq!(Rational::new(12, 0), None);
+    }
+
+    #[test]
     fn test_eq() {
-        let a = Rational { numerator: 120, denominator: 70 };
-        let b = Rational { numerator: 48, denominator: 28 };
+        let a = Rational::new(120, 70).expect("denominator shouldn't be zero");
+        let b = Rational::new(48, 28).expect("denominator shouldn't be zero");
         assert_eq!(a, b);
     }
 
     #[test]
     fn test_ord() {
-        let a = Rational { numerator: 8, denominator: 9 };
-        let b = Rational { numerator: 10, denominator: 11 };
+        let a = Rational::new(8, 9).expect("denominator shouldn't be zero");
+        let b = Rational::new(10, 11).expect("denominator shouldn't be zero");
         assert!(a < b);
         assert!(a <= b);
     }
 
     #[test]
     fn test_add() {
-        let a = Rational { numerator: 5, denominator: 8 };
-        let b = Rational { numerator: 1, denominator: 6 };
-        assert_eq!(a + b, Rational { numerator: 19, denominator: 24 });
+        let a = Rational::new(5, 8).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
+        assert_eq!(a + b, Rational::new(19, 24).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_add_assign() {
-        let mut a = Rational { numerator: 3, denominator: 4 };
-        let b = Rational { numerator: 1, denominator: 6 };
+        let mut a = Rational::new(3, 4).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
         a += b;
-        assert_eq!(a, Rational { numerator: 11, denominator: 12 });
+        assert_eq!(a, Rational::new(11, 12).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_sub() {
-        let a = Rational { numerator: 5, denominator: 8 };
-        let b = Rational { numerator: 1, denominator: 6 };
-        assert_eq!(a - b, Rational { numerator: 11, denominator: 24 });
+        let a = Rational::new(5, 8).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
+        assert_eq!(a - b, Rational::new(11, 24).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_sub_assign() {
-        let mut a = Rational { numerator: 3, denominator: 4 };
-        let b = Rational { numerator: 1, denominator: 6 };
+        let mut a = Rational::new(3, 4).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
         a -= b;
-        assert_eq!(a, Rational { numerator: 7, denominator: 12 });
+        assert_eq!(a, Rational::new(7, 12).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_mul() {
-        let a = Rational { numerator: 5, denominator: 8 };
-        let b = Rational { numerator: 1, denominator: 6 };
-        assert_eq!(a * b, Rational { numerator: 5, denominator: 48 });
+        let a = Rational::new(5, 8).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
+        assert_eq!(a * b, Rational::new(5, 48).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_mul_assign() {
-        let mut a = Rational { numerator: 3, denominator: 4 };
-        let b = Rational { numerator: 1, denominator: 6 };
+        let mut a = Rational::new(3, 4).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
         a *= b;
-        assert_eq!(a, Rational { numerator: 1, denominator: 8 });
+        assert_eq!(a, Rational::new(1, 8).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_div() {
-        let a = Rational { numerator: 5, denominator: 8 };
-        let b = Rational { numerator: 1, denominator: 6 };
-        assert_eq!(a / b, Rational { numerator: 15, denominator: 4 });
+        let a = Rational::new(5, 8).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
+        assert_eq!(a / b, Rational::new(15, 4).expect("denominator shouldn't be zero"));
     }
 
     #[test]
     fn test_div_assign() {
-        let mut a = Rational { numerator: 3, denominator: 4 };
-        let b = Rational { numerator: 1, denominator: 6 };
+        let mut a = Rational::new(3, 4).expect("denominator shouldn't be zero");
+        let b = Rational::new(1, 6).expect("denominator shouldn't be zero");
         a /= b;
-        assert_eq!(a, Rational { numerator: 9, denominator: 2 });
+        assert_eq!(a, Rational::new(9, 2).expect("denominator shouldn't be zero"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_div_by_zero() {
+        let a = Rational::new(12, 5).expect("denominator shouldn't be zero");
+        let b = Rational::new(0, 1).expect("denominator shouldn't be zero");
+        let _ = a / b;
     }
 }
